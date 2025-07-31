133c2b23927ade9dafe1e7fa49aed5c0fa29953c
Nicolas DEL PPRATO <delpratonicolas@outlook.fr>
2025-07-31 09:52:37 +0200

    Initial Commit

---
+++ TestTaHoma/TestTaHomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaHomaApp.swift
+//  TestTaHoma
+//
+//  Created by DEL PRATO Nicolas on 31/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaHomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestTaHomaTests/TestTaHomaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaHomaTests.swift
+//  TestTaHomaTests
+//
+//  Created by DEL PRATO Nicolas on 31/07/2025.
+//
+
+import Testing
+@testable import TestTaHoma
+
+struct TestTaHomaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
